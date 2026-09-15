930ebadb160842d0f73f3bcf83904688ac855c04
Italo Quimen <italo.quimen@outlook.com>
2026-09-15 12:47:59 -0300

    feat(backend): agregar repositorio JPA de personas

---
+++ backend/src/main/java/com/imqh/personas/repository/PersonaRepository.java
@@ -0,0 +1,7 @@
+package com.imqh.personas.repository;
+
+import com.imqh.personas.domain.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
+}
